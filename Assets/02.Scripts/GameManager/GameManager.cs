089004984824c842a479eb4cfd75a65dddce1a50
gugmin <149454489+gugmin@users.noreply.github.com>
2024-01-02 14:53:28 +0900

    [Add] GameManager
    게임매니저 싱글톤화

--- Assets/02.Scripts/GameManager/GameManager.cs
+++ Assets/02.Scripts/GameManager/GameManager.cs
@@ -1,16 +1,48 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("GameManager");
+                go.AddComponent<GameManager>();
+                _instance = go.GetComponent<GameManager>();
+                DontDestroyOnLoad(go);
+            }
+            return _instance;
+        }
+        set
+        {
+            if (_instance == null) _instance = value;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            if (_instance != this) Destroy(this);
+        }
+    }
+
+        void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
