9845a082e478ea80e89a8002149e9fa6dadc7d00
gugmin <149454489+gugmin@users.noreply.github.com>
2024-01-03 00:36:15 +0900

    [Add] EnemyMove
    페트롤 구현

--- Assets/02.Scripts/02.Enemy/EnemyMove.cs
+++ Assets/02.Scripts/02.Enemy/EnemyMove.cs
@@ -4,19 +4,57 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    private float speed;
+    Vector3 dirVec = Vector3.right;
+    [SerializeField] private float speed;
+    private int moveCount;
     private float waitTime;
-    private Transform spot;
-    private Rigidbody2D _rigidbody;
 
-    void Start()
+    private Rigidbody2D _rigidbody;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _anim;
+
+    private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        waitTime += Time.deltaTime;
+        Move();
+    }
+
+    private void Move()
+    {
+        _rigidbody.velocity = dirVec * speed;
+        ChangeMove();
+    }
+
+    private void ChangeMove()
+    {
+        _anim.SetBool("Run", true);
+        if (waitTime >= 5f)
+        {
+            _spriteRenderer.flipX = !_spriteRenderer.flipX;
+            moveCount++;
+            if (moveCount % 2 == 0)
+            {
+                dirVec = Vector3.right;
+                moveCount = 0;
+            }
+            else
+            {
+                dirVec = Vector3.left;
+            }
+            waitTime = 0;
+        }
+        else if (waitTime >= 3f)
+        {
+            _anim.SetBool("Run", false);
+            dirVec = Vector3.zero;
+        }
+
     }
 }
