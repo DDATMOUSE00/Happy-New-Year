5724bdb3416100c6d19bbeb0297f6c294842b3d0
DDATMOUSE00 <ddatmouse00@gmail.com>
2024-01-04 20:32:41 +0900

    플레이어 이동 세부
    좌우 반전, 바닥에서만 점프 가능

--- Assets/02.Scripts/01.Player/02.Controllers/PlayerMovement.cs
+++ Assets/02.Scripts/01.Player/02.Controllers/PlayerMovement.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using static UnityEngine.InputSystem.LowLevel.InputStateHistory;
 
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerController _controller;
-
     private Vector2 _movementDirection = Vector2.zero;
     private Rigidbody2D _rigidbody;
+    private bool IsGrounded = false;
+    private bool IsJumping = false;
+
+    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private LayerMask GroundLayer;
+
 
     private void Awake()
     {
@@ -20,6 +26,20 @@ public class PlayerMovement : MonoBehaviour
         _controller.OnMoveEvent += Move;
         _controller.OnJumpEvent += Jump;
     }
+    private void FixedUpdate()
+    {
+        CheckGrounded();
+        ApplyMovment(_movementDirection);
+
+        if (IsGrounded)
+        {
+            IsJumping = false;
+        }
+    }
+    private void CheckGrounded()
+    {
+        IsGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.2f, GroundLayer);
+    }
     private void Move(Vector2 direction)
     {
         _movementDirection = direction;
@@ -29,17 +49,24 @@ public class PlayerMovement : MonoBehaviour
         direction = direction * 5;
         Vector2 currentVelocity = _rigidbody.velocity;
         currentVelocity.x = direction.x;
-
         _rigidbody.velocity = currentVelocity;
+
+        if (direction.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (direction.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
     private void Jump()
     {
-        _rigidbody.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
-
+        if (IsGrounded && !IsJumping)
+        {
+            _rigidbody.AddForce(Vector2.up * 13, ForceMode2D.Impulse);
+            IsJumping = true;
+        }
         Debug.Log("OnJump2" + ToString());
-    }
-    private void FixedUpdate()
-    {
-        ApplyMovment(_movementDirection);
     }
 }
