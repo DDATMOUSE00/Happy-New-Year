b575888a27fe9bb6a4ea3f9d786a171ca87bd1bf
Lawrence1031h <lawrence1031h@gmail.com>
2024-01-05 11:20:31 +0900

    [Add] 보스전 맵 추가
    보스전 맵 추가
    미니맵 카메라 사이즈 조절
    비밀 발판 추가 및 스크립트 추가

---
+++ Assets/02.Scripts/Maps/Hidenground.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hidenground : MonoBehaviour
+{
+    public GameObject hidenGround;
+
+    private void Awake()
+    {
+        hidenGround.SetActive(false);
+    }
+
+    // 보스가 죽었을 때, 이 발판을 true되게
+
+}
