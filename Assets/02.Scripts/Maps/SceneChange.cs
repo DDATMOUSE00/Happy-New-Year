a50471a01e507dab780ac601f12916b1d822430b
Lawrence1031h <lawrence1031h@gmail.com>
2024-01-08 16:36:16 +0900

    [Fix] Scene Fix
    merge for Fix

--- Assets/02.Scripts/Maps/SceneChange.cs
+++ Assets/02.Scripts/Maps/SceneChange.cs
@@ -9,10 +9,8 @@ public class SceneChange : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Entered");
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player Entered");
             SceneManager.LoadScene(nextSceneName);
         }
     }
