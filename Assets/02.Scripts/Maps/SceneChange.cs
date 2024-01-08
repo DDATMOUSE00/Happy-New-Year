e45a7d01554535f4be537efa559a9f53f3b161e4
Lawrence1031h <lawrence1031h@gmail.com>
2024-01-08 20:31:06 +0900

    [Fix] SceneChange 관련
    씬 전환 시 플레이어 위치 이동, 카메라 크기 수정

--- Assets/02.Scripts/Maps/SceneChange.cs
+++ Assets/02.Scripts/Maps/SceneChange.cs
@@ -6,12 +6,20 @@ using UnityEngine.SceneManagement;
 public class SceneChange : MonoBehaviour
 {
     public string nextSceneName;
+    public Vector3 nextScenePlayerPosition;
+
+    public Vector2 nextSceneCameraCenter;
+    public Vector2 nextSceneCameraMapSize;
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             SceneManager.LoadScene(nextSceneName);
+            other.transform.position = nextScenePlayerPosition;
+            CameraController.Instance.center = nextSceneCameraCenter;
+            CameraController.Instance.mapSize = nextSceneCameraMapSize;
         }
     }
 }
