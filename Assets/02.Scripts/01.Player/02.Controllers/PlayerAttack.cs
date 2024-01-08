aae4a74942a9ae42ea0ad1a13109e63ca90294fd
DDATMOUSE00 <ddatmouse00@gmail.com>
2024-01-08 17:40:34 +0900

    [fix] 플레이어 공격 구현
    플레이어 공격 구현완료

--- Assets/02.Scripts/01.Player/02.Controllers/PlayerAttack.cs
+++ Assets/02.Scripts/01.Player/02.Controllers/PlayerAttack.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
     private PlayerController _controller;
-    private float AttackTime;
-    public Animator anim;
+    private Animator anim;
+    public Transform AttackRangeBox;
+    public Vector2 AttackRangeBoxSize;
+    public Health Health;
+
+    static int minDamage = 10;
+    static int maxDamage = 20;
+    private int PlayerDMG;
+    private Collider2D PlayerRangeCollider;
 
     private void Awake()
     {
@@ -17,28 +25,46 @@ public class PlayerAttack : MonoBehaviour
     {
         _controller.OnAttackEvent += Attack;
     }
+    private void Update()
+    {
+        Collider2D Playercollider = Physics2D.OverlapBox(AttackRangeBox.position, AttackRangeBoxSize, 0);
+        PlayerRangeCollider = Playercollider;
+        Debug.Log(PlayerRangeCollider.tag);
+    }
     private void Attack()
     {
         if (_controller.IsAttacking)
         {
-            StartCoroutine(StartAttack());
+            StartAttack();
             Debug.Log("공격키입력");
         }
     }
-    IEnumerator StartAttack()
+    private void StartAttack()
     {
         _controller.IsAttacking = true;
         anim.SetBool("IsAttack", true);
-        AttackTime = 0f;
-        yield return new WaitForSeconds(_controller.AttackDelay);
-        AttackDMG();
-        EndAttack();
+        AttackRange();
         Debug.Log("공격시작");
     }
-    private void AttackDMG()
+    private void AttackRange()
     {
-
+        PlayerDMG = Random.Range(minDamage, maxDamage + 1);
+        if (PlayerRangeCollider.CompareTag("Enemy"))
+        {
+            if (PlayerRangeCollider.TryGetComponent(out Health health))
+            {
+                health.TakeDamage(PlayerDMG);
+            }
+            Debug.Log("데미지: " + PlayerDMG);
+        }
+        Invoke("EndAttack",0.3f);
     }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(AttackRangeBox.position, AttackRangeBoxSize);
+    }
+
     private void EndAttack()
     {
         _controller.IsAttacking = false;
