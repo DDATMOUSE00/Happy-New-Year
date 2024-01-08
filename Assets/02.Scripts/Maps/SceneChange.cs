b834d4f3d9682947cc585e4afc1de4bb643e18eb
Lawrence1031h <lawrence1031h@gmail.com>
2024-01-08 12:30:36 +0900

    [Add] SceneChange 추가
    씬 이동을 위한 스크립트 추가

--- Assets/02.Scripts/Maps/SceneChange.cs
+++ Assets/02.Scripts/Maps/SceneChange.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
+    public string nextSceneName;
 
 
-
-
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entered");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player Entered");
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
 }
